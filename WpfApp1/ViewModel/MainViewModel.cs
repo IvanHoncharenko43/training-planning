d69e16d6e4a41225b91022845d0e0d1ac69415eb
IvanHoncharenko43 <ivangoncharenko200714@gmail.com>
2025-04-03 14:24:54 +0300

    Add a structure for registration

--- WpfApp1/ViewModel/MainViewModel.cs
+++ WpfApp1/ViewModel/MainViewModel.cs
@@ -1,3 +1,4 @@
+using System.Security;
 using WpfApp1.Core;
 using WpfApp1.Services;
 using WpfApp1.ViewModel;
@@ -5,6 +6,28 @@ namespace WpfApp1.ViewModel;
 
 public class MainViewModel : Core.ViewModel
 {
+    private string _loginEmail;
+    public string LoginEmail
+    {
+        get => _loginEmail;
+        set
+        {
+            _loginEmail = value;
+            OnPropertyChanged();
+        }
+    }
+    private string _loginPassword;
+    public string LoginPassword
+    {
+        get => _loginPassword;
+        set
+        {
+            _loginPassword = value;
+            OnPropertyChanged();
+        }
+    }
+    public RelayCommand LoginCommand { get; set; }
+    public RelayCommand RegisterCommand { get; set; }
     private INavigationService _navigationService;
     public RelayCommand NavigateToLogin { get; set; }
     
@@ -21,5 +44,30 @@ public class MainViewModel : Core.ViewModel
     {
         NavigationService = navigationService;
         NavigateToLogin = new RelayCommand(o => { NavigationService.NavigateTo<LoginViewModel>(); }, o => true);
+        LoginCommand = new RelayCommand(o => Login(), o => CheckLogin());
+        RegisterCommand = new RelayCommand(o => Register(), o => true);
+    }
+
+    private void Login()
+    {
+        if (string.IsNullOrWhiteSpace(LoginEmail) || string.IsNullOrWhiteSpace(LoginPassword))
+        {
+            return;  //пізніше допишу помилку
+        }
+        
+    }
+
+    private bool CheckLogin()
+    {
+        return false;
+    }
+
+    private void Register()
+    {
+        if (string.IsNullOrWhiteSpace(LoginEmail) || string.IsNullOrWhiteSpace(LoginPassword))
+        {
+            return;
+        }
+        
     }
 }
