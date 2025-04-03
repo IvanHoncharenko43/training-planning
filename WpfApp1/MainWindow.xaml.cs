77e508699503069d40f9e51059ade006522ebbf1
Ivanov-And <andrew1y1ivanov@gmail.com>
2025-04-03 10:55:21 +0300

    A

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,23 +1,49 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp1;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace LoginApp
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ToggleForm_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (LoginForm.Visibility == Visibility.Visible)
+            {
+                LoginForm.Visibility = Visibility.Collapsed;
+                RegisterForm.Visibility = Visibility.Visible;
+                FormTitle.Text = "Реєстрація";
+            }
+            else
+            {
+                LoginForm.Visibility = Visibility.Visible;
+                RegisterForm.Visibility = Visibility.Collapsed;
+                FormTitle.Text = "Вхід";
+            }
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && (textBox.Text == "Email" || textBox.Text == "Ім'я"))
+            {
+                textBox.Text = "";
+            }
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // логіку входу судазес
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            // логіка реєстрації судазес
+        }
     }
 }
